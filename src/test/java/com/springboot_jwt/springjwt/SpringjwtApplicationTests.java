54de13eae8dccf45215079451026c184003f8a18
suraj <surajrace21@gmail.com>
2024-09-03 20:42:03 +0530

    modal-controllers-services-initalized

---
+++ src/test/java/com/springboot_jwt/springjwt/SpringjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_jwt.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
